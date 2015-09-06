1c1e981c6d0fe5418c034d7aba11afa6fdcb39bc
NEKOTENI <riverfield.tk@gmail.com>
2015-09-06 11:22:47 +0900

    モデルのテストを書いてみた

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,4 +2,6 @@ class Memo < ActiveRecord::Base
   belongs_to :user
   has_many :taggings
   has_many :tags, :through=>:taggings
+
+  # メモが空だったらエラーメッセージを出すバリデーションを書くこと。
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 自動生成されたものを一旦コメントアウトしておく
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.new }
+
+  describe 'validation' do
+    describe 'email' do
+      it { expect(user).to validate_presence_of(:email) }
+      it { expect(user).to validate_uniqueness_of(:email) }
+    end
+
+    describe 'password' do
+      it { expect(user).to validate_length_of(:password).is_at_least(3) }
+    end
+
+    describe 'password_confirmation' do
+      it { expect(user).to validate_presence_of(:password_confirmation) }
+    end
+  end
 end
