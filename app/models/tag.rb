99790446761c4a96c06b2055124855dd84bcc131
NEKOTENI <riverfield.tk@gmail.com>
2015-09-06 18:37:22 +0900

    tagモデルにvalidationを追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :memos, :through=>:taggings
+
+  # タグ名が空でないか？
+  validates :tag, presence: true
+  validates :user_id, presence: true
 end
