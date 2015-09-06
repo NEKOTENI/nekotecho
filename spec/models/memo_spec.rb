624f5e32fb4a94cedf5a396b35d55f11d26e82f5
NEKOTENI <riverfield.tk@gmail.com>
2015-09-06 18:45:35 +0900

    自動生成されたテストコードを削除

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 describe Memo do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:memo) { Memo.new }
+
+  describe 'validation' do
+    describe 'ねこメモ' do
+      it { expect(memo).to validate_presence_of(:text) }
+    end
+
+    describe 'user_id' do
+      it { expect(memo).to validate_presence_of(:user_id)}
+    end
+  end
 end
