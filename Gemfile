ff7ddba47c04c055d613564308bad27c411f144b
NEKOTENI <riverfield.tk@gmail.com>
2015-09-20 13:44:47 +0900

    PaperClipを追加

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootstrap-sass', '~> 3.3.5'
 
 gem 'sorcery'
 
+gem 'paperclip'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,6 +3,12 @@ class Memo < ActiveRecord::Base
   has_many :taggings
   has_many :tags, :through=>:taggings
 
+  # PaperClip用の設定
+  has_attached_file :image,
+                    :styles => { medium: "100×100>" }
+  validates_attachment_content_type :image,
+                                    :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
   # メモが空でないか？
   validates :text, presence: true
   validates :user_id, presence: true
